521ba0c26e629b18994fb9ad10aa33dfb499335f
Sanchir01 <emgushovs@mail.ru>
2025-02-21 17:06:47 +0300

    add footer

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -31,26 +31,41 @@ const footerColumnContent: IFooterColumn[] = [
 			{ title: 'Careers', href: '/careers' },
 			{ title: 'Partnership', href: '/partnership' }
 		]
+	},
+	{
+		links: [
+			{ title: 'Our Stores', href: '/stores' },
+			{ title: 'Contact Info', href: '/contacts' },
+			{ title: 'Careers', href: '/careers' }
+		]
 	}
 ]
 
 const Footer = () => {
 	return (
-		<footer className='bg-custom-black text-white mt-30 p-10'>
+		<footer className='bg-custom-black text-white mt-30 p-10 uppercase '>
 			<div className='container'>
-				<div className='flex justify-between'>
-					<div className='flex gap-10'>
-						{footerColumnContent.map((item, i) => (
-							<ul key={i}>
-								{item.links.map((content, i) => (
-									<li key={i}>
-										<a href={content.href}>{content.title}</a>
-									</li>
-								))}
-							</ul>
-						))}
+				<div className='flex justify-between gap-5'>
+					<div className='flex flex-col justify-between'>
+						<div className='flex gap-10 font-normal'>
+							{footerColumnContent.map((item, i) => (
+								<ul key={i}>
+									{item.links.map((content, i) => (
+										<li key={i}>
+											<a href={content.href}>{content.title}</a>
+										</li>
+									))}
+								</ul>
+							))}
+						</div>
+						<p className='mt-25'>© 2024 Все права защищены</p>
 					</div>
-					<Title text={'пространстов, стиль, порядок'} />
+
+					<Title
+						text={'пространсто, стиль, порядок'}
+						size='xs'
+						className='font-gilroy text-[100px] opacity-10 leading-[110px]'
+					/>
 				</div>
 			</div>
 		</footer>
